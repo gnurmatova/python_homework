64afaaa1ade19d1cd4107e3dc8ae18424efc6bee
Gula Nurmatova <gnurmatova@icloud.com>
2020-10-18 06:30:22 -0400

    Update test_midterm.py

--- midterm/test_midterm.py
+++ midterm/test_midterm.py
@@ -1,11 +1,11 @@
-from midterm.py import converter
+from midterm import converter
 
 def test_currencyConverter():
-    assert converter("30", "USD", "USD") == "30.0 in USD = 30.0 in USD"
-    assert converter("30", "INR", "INR") == "30.0 in INR = 30.0 in INR"
-    assert converter("30", "ert", "USD") == "ERROR: Invalid API endpoint"
-    assert converter("30", "USD", "ert") == "Country code(s) doesn't exist"
-    assert converter("30", "USD", "TTT") == "Country code(s) doesn't exist"
-    assert converter("30", "TTT", "USD") == "ERROR: Invalid API endpoint"
+    assert converter( "USD", "USD", "30") == "30 in USD = 30 in USD"
+    assert converter( "INR", "INR", "30") == "30 in INR = 30 in INR"
+    assert converter( "ert", "USD", "30") == "ERROR: Invalid API endpoint"
+    assert converter( "USD", "ert", "30") == "Country code(s) doesn't exist"
+    assert converter( "USD", "TTT", "30") == "Country code(s) doesn't exist"
+    assert converter( "TTT", "USD", "30") == "ERROR: Invalid API endpoint"
 
 test_currencyConverter()
